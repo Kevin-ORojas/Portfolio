b001ed31f3fd2c79a5a71264b6a2294f2e1850df
Kevin-ORojas <kevinroj326@gmail.com>
2025-08-26 10:02:32 -0500

    sidebar en proceso

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,17 @@
+import { Github } from "lucide-react";
+
 export default function Footer() {
   return (
-    <div className="flex gap-4">
-      <p>git</p>
-      <p>git</p>
-    </div>
+    <main className="flex gap-4 p-2">
+      <section className="hover:bg-indigo-500 text-white border-l-4 w-full cursor-pointer">
+        <a
+          href="https://github.com/Kevin-ORojas"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Github />
+        </a>
+      </section>
+    </main>
   );
 }
